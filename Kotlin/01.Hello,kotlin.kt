05cd96341cbce7102bba6e04667d260d679916dc
kimjinsub <hikm9985@naver.com>
2022-01-13 03:42:18 +0900

    Hello Kotlin

--- Kotlin/01.Hello,kotlin.kt
+++ Kotlin/01.Hello,kotlin.kt
@@ -1,9 +1,12 @@
 package com.example.kotlinstudy.Kotlin
 /*
-    fun은 function의 약자로 함수를 선언하는 키워드이다.
-    fun 함수명(변수명:타입):리턴타입
-    println:출력 후 다음줄로 이동
-    print : 다음줄로 이동 안함
+    fun: function의 약자로 함수를 선언하는 키워드
+    main: 함수명으로 시작과 끝을 나타낸다.{}안에 명령어를 작성한다.
+    args: 인수의 이름
+    Array<String>: 인수의 유형 String의 배열
+    Unit: 리턴타입 추론이 가능하여 생락이 가능하다.
+    println: 텍스트를 출력하는 함수 ""안에서 텍스트를 적어준다.
+    print: println과 비슷하지만 이 함수는 텍스트가 다음 줄로 이동을 안함
  */
 fun main(args:Array<String>):Unit{
     println("Hello,Kotlin!!")
