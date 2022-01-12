f0149440172256758f060a8672f48932379715c3
kimjinsub <hikm9985@naver.com>
2022-01-13 03:38:56 +0900

    Hellow,kotlin

---
+++ Kotlin/01.Hello,kotlin.kt
@@ -0,0 +1,10 @@
+package com.example.kotlinstudy.Kotlin
+/*
+    fun은 function의 약자로 함수를 선언하는 키워드이다.
+    fun 함수명(변수명:타입):리턴타입
+    println:출력 후 다음줄로 이동
+    print : 다음줄로 이동 안함
+ */
+fun main(args:Array<String>):Unit{
+    println("Hello,Kotlin!!")
+}
